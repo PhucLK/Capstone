c1883bc8869e98d8d641c6baa8494c20d979eba5
phuclk <phuclkcarryonto04@gmail.com>
2022-12-07 19:53:25 +0700

    completed

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '9pbpys8n3a'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = '2o0nlz1xh5'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev3`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
